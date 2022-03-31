7b773dc37a76116e42de4e9119300b1e58dbc40e
nikitademedyuk1@gmail.com <nikitademedyuk1@gmail.com>
2022-03-31 08:25:18 +0300

    Initial Commit

---
+++ HW2-T10/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HW2-T10
+//
+//  Created by Никита on 3/31/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
